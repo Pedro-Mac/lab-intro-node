2537c4ed2fe65073090b1c101b0715ddc1283df7
KozaX <55134615+KozaX@users.noreply.github.com>
2020-07-20 16:01:51 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    //Iteration 1
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  //Iteration 2
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if (pos > this.items.length - 1) {
+      throw new Error('OutOfBounds');
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  max() {}
+  max() {
+    let maxNum = -Infinity;
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList');
+    }
+    for (let el of this.items) {
+      if (el > maxNum) {
+        maxNum = el;
+      }
+    }
 
-  min() {}
+    return maxNum;
+  }
 
-  sum() {}
+  min() {
+    let minNum = Infinity;
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList');
+    }
+    for (let el of this.items) {
+      if (el < minNum) {
+        minNum = el;
+      }
+    }
 
-  avg() {}
+    return minNum;
+  }
+
+  sum() {
+    let sum;
+    if (this.items.length === 0) {
+      sum = 0;
+    } else {
+      sum = this.items.reduce((acc, val) => {
+        return acc + val;
+      }, 0);
+    }
+    return sum;
+  }
+
+  avg() {
+    let avg;
+    if (this.items.length < 1) {
+      throw new Error('EmptySortedList');
+    } else {
+      avg =
+        this.items.reduce((acc, val) => {
+          return acc + val;
+        }, 0) / this.items.length;
+    }
+    return avg;
+  }
 }
 
 module.exports = SortedList;
